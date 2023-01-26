85dbc9764fdf62031e4f67555b7a19c8f9dcfdb6
Tarun Gopalkrishna A <tarungopalkrishna@gmail.com>
2023-01-27 00:06:55 +0530

    update: added comments on how to make this service distributed

--- main.go
+++ main.go
@@ -4,6 +4,36 @@ package main
 // Be careful when the database is sharded
 // If the deployment is a sharded cluster, a shard removal may cause an open change stream cursor to close, and the closed change stream cursor may not be fully resumable.
 
+// How can I make this service distributed?
+/*
+CHATGPT:
+There are a few different ways you could ensure that only one replica of your service is streaming data from Mongo
+and updating Elasticsearch at a time when you scale up the replicas in Kubernetes:
+
+1. Use a leader election mechanism: This would involve having each replica of your service compete
+   to become the leader, and only the leader would be responsible for streaming data from Mongo and
+   updating Elasticsearch. This can be done using Kubernetes primitives like ConfigMaps or using a
+   separate leader election library like ZooKeeper.
+
+2. Use a shared lock: This would involve having each replica of your service acquire a lock
+   (e.g. using a distributed key-value store like etcd) before streaming data from Mongo and
+   updating Elasticsearch. Only one replica would be able to acquire the lock at a time,
+   ensuring that only one replica is streaming data at a time.
+
+3. Use a shared queue: One replica can stream data and put it in a shared queue. Other replicas
+   can dequeue it and update elasticsearch.
+
+4. Use a shared state: You can store a shared state in a shared kubernetes configMap, that can be
+   updated by any replica and read by all replicas. If the shared state indicates that streaming
+   is already in progress, a new replica can wait for the streaming to complete before starting to
+   stream data.
+
+5. Use Kubernetes Pod Anti-Affinity: This would configure the Kubernetes scheduler to avoid scheduling
+   multiple replicas of your service on the same node. This would ensure that only one replica would be
+   running on each node, and therefore only one replica would be streaming data from Mongo and updating
+   Elasticsearch.
+*/
+
 import (
 	"bytes"
 	"context"
