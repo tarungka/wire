f2eb36675539d7cfcd3d61b0e8ab5aeeaf9bdab8
Tarun Gopalkrishna A <tarungopalkrishna@gmail.com>
2024-10-10 00:08:13 +0530

    build: added build file

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Get the current branch name
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+# Get the latest commit hash
+COMMIT=$(git rev-parse --short HEAD)
+
+# Get the current date
+DATE=$(date +"%Y-%m-%dT%H:%M:%S")
+
+# Get the version: the latest tag or commit hash if no tag is present
+VERSION=$(git describe --tags --always --dirty)
+
+# Build the binary with dynamic ldflags
+go build -o wire -ldflags=" \
+    -w -s \
+    -X github.com/tarungka/wire/internal/cmd.CompilerCommand=musl-gcc \
+    -X github.com/tarungka/wire/internal/cmd.Version=${VERSION} \
+    -X github.com/tarungka/wire/internal/cmd.Branch=${BRANCH} \
+    -X github.com/tarungka/wire/internal/cmd.Commit=${COMMIT} \
+    -X github.com/tarungka/wire/internal/cmd.Buildtime=${DATE}" ./cmd/.
