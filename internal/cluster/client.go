83c9b044238292d50d1fdaeb999e380111d70683
Tarun Gopalkrishna A <tarungopalkrishna@gmail.com>
2025-12-29 09:16:36 +0530

    fix: the dialer

--- internal/cluster/client.go
+++ internal/cluster/client.go
@@ -1,6 +1,8 @@
 package cluster
 
 import (
+	"crypto/tls"
+	"encoding/binary"
 	"errors"
 	"io"
 	"net"
@@ -8,11 +10,14 @@ import (
 	"time"
 
 	"github.com/rqlite/rqlite/v8/auth"
+	"github.com/rqlite/rqlite/v8/rtls"
 	"github.com/rs/zerolog"
-	"github.com/tarungka/wire/internal/cluster/proto"
+	clstrPB "github.com/tarungka/wire/internal/cluster/proto"
 	command "github.com/tarungka/wire/internal/command/proto"
+	"github.com/tarungka/wire/internal/logger"
 	"github.com/tarungka/wire/internal/tcp"
 	"github.com/tarungka/wire/internal/tcp/pool"
+	pb "google.golang.org/protobuf/proto"
 )
 
 const (
@@ -28,13 +33,31 @@ var errNotImplemented = errors.New("not implemented")
 // CreateRaftDialer creates a dialer for connecting to other nodes' Raft service. If the cert and
 // key arguments are not set, then the returned dialer will not use TLS.
 func CreateRaftDialer(cert, key, caCert, serverName string, Insecure bool) (*tcp.Dialer, error) {
-	return nil, errNotImplemented
+	var tlsConfig *tls.Config
+	var err error
+	if cert != "" || key != "" || caCert != "" {
+		tlsConfig, err = rtls.CreateClientConfig(cert, key, caCert, serverName, Insecure)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return tcp.NewDialer(MuxRaftHeader, tlsConfig), nil
 }
 
 // CredentialsFor returns a Credentials instance for the given username, or nil if
 // the given CredentialsStore is nil, or the username is not found.
-func CredentialsFor(credStr *auth.CredentialsStore, username string) *proto.Credentials {
-	return nil
+func CredentialsFor(credStr *auth.CredentialsStore, username string) *clstrPB.Credentials {
+	if credStr == nil {
+		return nil
+	}
+	password, ok := credStr.Password(username)
+	if !ok {
+		return nil
+	}
+	return &clstrPB.Credentials{
+		Username: username,
+		Password: password,
+	}
 }
 
 // Client allows communicating with a remote node.
@@ -54,98 +77,155 @@ type Client struct {
 }
 
 // NewClient returns a client instance for talking to a remote node.
-// Clients will retry certain commands if they fail, to allow for
-// remote node restarts. Cluster management operations such as joining
-// and removing nodes are not retried, to make it clear to the operator
-// that the operation failed. In addition, higher-level code will
-// usually retry these operations.
 func NewClient(dl Dialer, t time.Duration) *Client {
-	return nil
+	return &Client{
+		dialer:  dl,
+		timeout: t,
+		pools:   make(map[string]pool.Pool),
+		logger:  logger.GetLogger("cluster-client"),
+	}
 }
 
 // SetLocal informs the client instance of the node address for the node
 // using this client. Along with the Service instance it allows this
 // client to serve requests for this node locally without the network hop.
 func (c *Client) SetLocal(nodeAddr string, serv *Service) error {
-	return errNotImplemented
+	c.localMu.Lock()
+	defer c.localMu.Unlock()
+	c.localNodeAddr = nodeAddr
+	c.localServ = serv
+	return nil
+}
+
+// Join joins this node to a cluster at the remote address nodeAddr.
+func (c *Client) Join(jr *command.JoinRequest, nodeAddr string, creds *clstrPB.Credentials, timeout time.Duration) error {
+	cmd := &clstrPB.Command{
+		Type: clstrPB.Command_COMMAND_TYPE_JOIN,
+		Request: &clstrPB.Command_JoinRequest{
+			JoinRequest: jr,
+		},
+		Credentials: creds,
+	}
+
+	_, _, err := c.retry(cmd, nodeAddr, timeout, noRetries)
+	return err
 }
 
 // GetNodeAPIAddr retrieves the API Address for the node at nodeAddr
 func (c *Client) GetNodeAPIAddr(nodeAddr string, retries int, timeout time.Duration) (string, error) {
-	return "", errNotImplemented
+	cmd := &clstrPB.Command{
+		Type: clstrPB.Command_COMMAND_TYPE_GET_NODE_API_URL,
+	}
+	p, _, err := c.retry(cmd, nodeAddr, timeout, retries)
+	if err != nil {
+		return "", err
+	}
+
+	meta := &clstrPB.NodeMeta{}
+	if err := pb.Unmarshal(p, meta); err != nil {
+		return "", err
+	}
+	return meta.Url, nil
 }
 
-// GetCommitIndex retrieves the commit index for the node at nodeAddr
-func (c *Client) GetCommitIndex(nodeAddr string, retries int, timeout time.Duration) (uint64, error) {
-	return 0, errNotImplemented
+// retry retries a command on a remote node.
+func (c *Client) retry(command *clstrPB.Command, nodeAddr string, timeout time.Duration, maxRetries int) ([]byte, int, error) {
+	for i := 0; i <= maxRetries; i++ {
+		conn, err := c.getConn(nodeAddr)
+		if err != nil {
+			return nil, i, err
+		}
+
+		if err := writeCommand(conn, command, timeout); err != nil {
+			conn.Close()
+			continue
+		}
+
+		p, err := readResponse(conn, timeout)
+		if err != nil {
+			conn.Close()
+			continue
+		}
+
+		conn.Close() // Return to pool if it was a pool conn
+		return p, i, nil
+	}
+	return nil, maxRetries, errors.New("max retries exceeded")
 }
 
-// Execute performs an Execute on a remote node. If username is an empty string
-// no credential information will be included in the Execute request to the
-// remote node.
-func (c *Client) Execute(er *command.ExecuteRequest, nodeAddr string, creds *proto.Credentials, timeout time.Duration, retries int) ([]*command.ExecuteQueryResponse, error) {
-	return nil, errNotImplemented
+func (c *Client) getConn(nodeAddr string) (net.Conn, error) {
+	c.poolMu.Lock()
+	p, ok := c.pools[nodeAddr]
+	if !ok {
+		var err error
+		p, err = pool.NewChannelPool(maxPoolCapacity, func() (net.Conn, error) {
+			return c.dialer.Dial(nodeAddr, c.timeout)
+		})
+		if err != nil {
+			c.poolMu.Unlock()
+			return nil, err
+		}
+		c.pools[nodeAddr] = p
+	}
+	c.poolMu.Unlock()
+	return p.Get()
 }
 
-// Query performs a Query on a remote node.
-func (c *Client) Query(qr *command.QueryRequest, nodeAddr string, creds *proto.Credentials, timeout time.Duration) ([]*command.QueryRows, error) {
-	return nil, errNotImplemented
+func writeCommand(conn net.Conn, c *clstrPB.Command, timeout time.Duration) error {
+	p, err := pb.Marshal(c)
+	if err != nil {
+		return err
+	}
+
+	if timeout > 0 {
+		conn.SetWriteDeadline(time.Now().Add(timeout))
+	}
+
+	b := make([]byte, protoBufferLengthSize)
+	binary.LittleEndian.PutUint64(b, uint64(len(p)))
+	if _, err := conn.Write(b); err != nil {
+		return err
+	}
+	_, err = conn.Write(p)
+	return err
 }
 
-// Request performs an ExecuteQuery on a remote node.
-func (c *Client) Request(r *command.ExecuteQueryRequest, nodeAddr string, creds *proto.Credentials, timeout time.Duration, retries int) ([]*command.ExecuteQueryResponse, error) {
-	return nil, errNotImplemented
-}
+func readResponse(conn net.Conn, timeout time.Duration) ([]byte, error) {
+	if timeout > 0 {
+		conn.SetReadDeadline(time.Now().Add(timeout))
+	}
 
-// Backup retrieves a backup from a remote node and writes to the io.Writer
-func (c *Client) Backup(br *command.BackupRequest, nodeAddr string, creds *proto.Credentials, timeout time.Duration, w io.Writer) error {
-	return errNotImplemented
-}
+	b := make([]byte, protoBufferLengthSize)
+	if _, err := io.ReadFull(conn, b); err != nil {
+		return nil, err
+	}
+	sz := binary.LittleEndian.Uint64(b)
 
-// Load loads a BadgerDB file into the database.
-func (c *Client) Load(lr *command.LoadRequest, nodeAddr string, creds *proto.Credentials, timeout time.Duration, retries int) error {
-	return errNotImplemented
-}
-
-// RemoveNode removes a node from the cluster
-func (c *Client) RemoveNode(rn *command.RemoveNodeRequest, nodeAddr string, creds *proto.Credentials, timeout time.Duration) error {
-	return errNotImplemented
-}
-
-// Notify notifies a remote node that this node is ready to bootstrap.
-func (c *Client) Notify(nr *command.NotifyRequest, nodeAddr string, creds *proto.Credentials, timeout time.Duration) error {
-	return errNotImplemented
-}
-
-// Join joins this node to a cluster at the remote address nodeAddr.
-func (c *Client) Join(jr *command.JoinRequest, nodeAddr string, creds *proto.Credentials, timeout time.Duration) error {
-	return errNotImplemented
+	p := make([]byte, sz)
+	if _, err := io.ReadFull(conn, p); err != nil {
+		return nil, err
+	}
+	return p, nil
 }
 
 // Stats returns stats on the Client instance
-func (c *Client) Stats() (map[string]any, error) {
+func (c *Client) Stats() (map[string]interface{}, error) {
+	return nil, nil
+}
+
+// Unimplemented stubs to satisfy compiler for now if they are called elsewhere
+func (c *Client) Execute(er *command.ExecuteRequest, nodeAddr string, creds *clstrPB.Credentials, timeout time.Duration, retries int) ([]*command.ExecuteQueryResponse, error) {
 	return nil, errNotImplemented
 }
-
-func (c *Client) dial(nodeAddr string) (net.Conn, error) {
+func (c *Client) Query(qr *command.QueryRequest, nodeAddr string, creds *clstrPB.Credentials, timeout time.Duration) ([]*command.QueryRows, error) {
 	return nil, errNotImplemented
 }
-
-// retry retries a command on a remote node. It does this so we churn through connections
-// in the pool if we hit an error, as the remote node may have restarted and the pool's
-// connections are now stale.
-func (c *Client) retry(command *proto.Command, nodeAddr string, timeout time.Duration, maxRetries int) ([]byte, int, error) {
-	return nil, 0, errNotImplemented
+func (c *Client) Request(r *command.ExecuteQueryRequest, nodeAddr string, creds *clstrPB.Credentials, timeout time.Duration, retries int) ([]*command.ExecuteQueryResponse, error) {
+	return nil, errNotImplemented
 }
-
-// writeCommand writes command to the connection.
-func writeCommand(conn net.Conn, c *proto.Command, timeout time.Duration) error {
+func (c *Client) Notify(nr *command.NotifyRequest, nodeAddr string, creds *clstrPB.Credentials, timeout time.Duration) error {
 	return errNotImplemented
 }
-
-func readResponse(conn net.Conn, timeout time.Duration) (buf []byte, retErr error) {
-	return nil, errNotImplemented
-}
-
-func handleConnError(conn net.Conn) {
+func (c *Client) RemoveNode(rn *command.RemoveNodeRequest, nodeAddr string, creds *clstrPB.Credentials, timeout time.Duration) error {
+	return errNotImplemented
 }
--- internal/tcp/dialer.go
+++ internal/tcp/dialer.go
@@ -29,7 +29,23 @@ type Dialer struct {
 }
 
 // Dial dials the cluster service at the given addr and returns a connection.
-func (d *Dialer) Dial(addr string, timeout time.Duration) (conn net.Conn, retErr error) {
-	// TODO: Implementation truncated
-	return nil, nil
+func (d *Dialer) Dial(addr string, timeout time.Duration) (net.Conn, error) {
+	var conn net.Conn
+	var err error
+	if d.tlsConfig != nil {
+		conn, err = tls.DialWithDialer(&net.Dialer{Timeout: timeout}, "tcp", addr, d.tlsConfig)
+	} else {
+		conn, err = net.DialTimeout("tcp", addr, timeout)
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	// Write the header byte to the connection.
+	if _, err := conn.Write([]byte{d.header}); err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	return conn, nil
 }
