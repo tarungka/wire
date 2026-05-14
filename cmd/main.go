280f829ce005341d18668eee7b98832aec301337
Tarun Ashok <tarungopalkrishna@gmail.com>
2026-05-14 15:50:33 +0530

    refactor(cmd): make entrypoint more idiomatic

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,9 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"github.com/spf13/pflag"
 	"golang.org/x/sync/errgroup"
 
 	"github.com/tarungka/wire/internal/cmd"
@@ -29,6 +31,7 @@ Visit https://www.github.com/tarungka/wire to learn more.`
 	logFilePath           = "server.log"
 	shutdownTimeout       = 5 * time.Second
 	fallbackCoordinatorID = "wire-node-1"
+	fallbackWorkerID      = "wire-worker-1"
 
 	modeCoordinator = "coordinator"
 	modeWorker      = "worker"
@@ -50,31 +53,30 @@ func main() {
 	if err := run(); err != nil {
 		log.Fatal().Err(err).Msg("wire exited with error")
 	}
-	log.Info().Msg("Shutting down.")
 }
 
 func run() error {
-	sigCh := HandleSignals(syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	ctx, cancel := CreateContext(sigCh)
-	defer cancel()
-
-	logFile := openLogFile(logFilePath)
-	if logFile != nil {
-		defer func() { _ = logFile.Close() }()
-	}
-
-	cliCfg, flagSet, err := initFlags(name, desc, &BuildInfo{
+	build := &BuildInfo{
 		Version: cmd.Version,
 		Commit:  cmd.Commit,
 		Branch:  cmd.Branch,
-	})
+	}
+
+	cliOpts, flagSet, err := initFlags(name, desc)
 	if err != nil {
+		if errors.Is(err, pflag.ErrHelp) {
+			return nil
+		}
 		return fmt.Errorf("parse flags: %w", err)
+	}
+	if cliOpts.showVersion {
+		fmt.Fprintln(os.Stdout, versionString(name, build))
+		return nil
 	}
 
 	fmt.Print(logo)
 
-	wireCfg, err := config.Load(cliCfg.ConfigPath)
+	wireCfg, err := config.Load(cliOpts.configPaths)
 	if err != nil {
 		return fmt.Errorf("load config: %w", err)
 	}
@@ -85,42 +87,52 @@ func run() error {
 		return fmt.Errorf("validate config: %w", err)
 	}
 
-	logger.SetDevelopment(wireCfg.Node.Debug)
-	logger.SetLogFile(logFile)
-	log.Logger = logger.GetLogger("main")
-
-	if wireCfg.Node.Debug {
-		log.Debug().Int("pid", os.Getpid()).Int("ppid", os.Getppid()).Msg("debug mode")
+	logFile := openLogFile(logFilePath)
+	if logFile != nil {
+		defer func() { _ = logFile.Close() }()
 	}
 
-	shutdownObs, err := initObservability(ctx, &wireCfg)
+	logger.SetDevelopment(wireCfg.Node.Debug)
+	logger.SetLogFile(logFile)
+	appLog := logger.GetLogger("main")
+
+	ctx, stopSignals := signalContext(context.Background(), appLog, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	defer stopSignals()
+
+	if wireCfg.Node.Debug {
+		appLog.Debug().Int("pid", os.Getpid()).Int("ppid", os.Getppid()).Msg("debug mode")
+	}
+
+	nodeID := resolveRuntimeNodeID(&wireCfg)
+	shutdownObs, err := initObservability(ctx, appLog, &wireCfg, nodeID)
 	if err != nil {
 		return fmt.Errorf("init observability: %w", err)
 	}
 	defer shutdownObs()
 
-	log.Info().Str("mode", wireCfg.Mode).Msg("Starting wire...")
+	appLog.Info().Str("mode", wireCfg.Mode).Msg("Starting wire...")
 
-	runErr := dispatch(ctx, &wireCfg)
+	runErr := dispatch(ctx, appLog, &wireCfg)
 	if errors.Is(runErr, context.Canceled) {
 		return nil
 	}
 	return runErr
 }
 
-func dispatch(ctx context.Context, wireCfg *config.WireConfig) error {
+func dispatch(ctx context.Context, appLog zerolog.Logger, wireCfg *config.WireConfig) error {
 	switch wireCfg.Mode {
 	case modeWorker:
-		return runWorker(ctx, wireCfg)
+		return runWorker(ctx, appLog, wireCfg)
 	case modeCoordinator, "":
-		return runCoordinator(ctx, wireCfg)
+		return runCoordinator(ctx, appLog, wireCfg)
 	default:
 		return fmt.Errorf("unsupported mode %q", wireCfg.Mode)
 	}
 }
 
-func runCoordinator(ctx context.Context, wireCfg *config.WireConfig) error {
+func runCoordinator(ctx context.Context, appLog zerolog.Logger, wireCfg *config.WireConfig) error {
 	nodeID := resolveCoordinatorNodeID(wireCfg.Node.ID)
+	advertiseAddr := coordinatorAdvertiseAddr(wireCfg.HTTP)
 
 	store, err := coordinator.NewPebbleStore(wireCfg.Node.DataDir)
 	if err != nil {
@@ -128,7 +140,7 @@ func runCoordinator(ctx context.Context, wireCfg *config.WireConfig) error {
 	}
 	defer func() { _ = store.Close() }()
 
-	election, err := newLeaderElection(wireCfg.Election, wireCfg.HTTP.Addr)
+	election, err := newLeaderElection(wireCfg.Election, advertiseAddr)
 	if err != nil {
 		return err
 	}
@@ -136,11 +148,11 @@ func runCoordinator(ctx context.Context, wireCfg *config.WireConfig) error {
 	coord := coordinator.New(coordinator.CoordinatorConfig{
 		DataDir:    wireCfg.Node.DataDir,
 		NodeID:     nodeID,
-		ListenAddr: wireCfg.HTTP.Addr,
-	}, store, election, log.Logger)
+		ListenAddr: advertiseAddr,
+	}, store, election, appLog)
 
-	httpSrv := coordinator.NewHTTPServer(coord, wireCfg.HTTP.Addr, log.Logger)
-	transportSrv := coordinator.NewTransportServer(coord, wireCfg.Listen, log.Logger)
+	httpSrv := coordinator.NewHTTPServer(coord, wireCfg.HTTP.Addr, appLog)
+	transportSrv := coordinator.NewTransportServer(coord, wireCfg.Listen, appLog)
 
 	g, gCtx := errgroup.WithContext(ctx)
 
@@ -157,25 +169,25 @@ func runCoordinator(ctx context.Context, wireCfg *config.WireConfig) error {
 
 	g.Go(func() error {
 		<-gCtx.Done()
-		log.Info().Msg("Shutting down coordinator...")
+		appLog.Info().Msg("Shutting down coordinator...")
 		shutdownCtx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
 		defer cancel()
-		_ = coord.Shutdown(shutdownCtx)
-		_ = httpSrv.Shutdown(shutdownCtx)
-		_ = transportSrv.Shutdown(shutdownCtx)
+		logShutdownError(appLog, "coordinator", coord.Shutdown(shutdownCtx))
+		logShutdownError(appLog, "http server", httpSrv.Shutdown(shutdownCtx))
+		logShutdownError(appLog, "transport server", transportSrv.Shutdown(shutdownCtx))
 		return nil
 	})
 
 	return g.Wait()
 }
 
-func runWorker(ctx context.Context, wireCfg *config.WireConfig) error {
+func runWorker(ctx context.Context, appLog zerolog.Logger, wireCfg *config.WireConfig) error {
 	w := worker.New(worker.Config{
 		WorkerID:        wireCfg.Worker.WorkerID,
 		CoordinatorAddr: wireCfg.Worker.CoordinatorAddr,
 		ListenAddr:      wireCfg.Worker.ListenAddr,
 		TaskSlots:       wireCfg.Worker.TaskSlots,
-	}, log.Logger)
+	}, appLog)
 
 	g, gCtx := errgroup.WithContext(ctx)
 
@@ -183,7 +195,7 @@ func runWorker(ctx context.Context, wireCfg *config.WireConfig) error {
 
 	g.Go(func() error {
 		<-gCtx.Done()
-		log.Info().Msg("Shutting down worker...")
+		appLog.Info().Msg("Shutting down worker...")
 		shutdownCtx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
 		defer cancel()
 		return w.Shutdown(shutdownCtx)
@@ -213,6 +225,32 @@ func resolveCoordinatorNodeID(configured string) string {
 	return fallbackCoordinatorID
 }
 
+func resolveRuntimeNodeID(wireCfg *config.WireConfig) string {
+	if wireCfg.Mode == modeWorker {
+		if wireCfg.Worker.WorkerID != "" {
+			return wireCfg.Worker.WorkerID
+		}
+		if hostname, _ := os.Hostname(); hostname != "" {
+			return hostname
+		}
+		return fallbackWorkerID
+	}
+	return resolveCoordinatorNodeID(wireCfg.Node.ID)
+}
+
+func coordinatorAdvertiseAddr(cfg config.HTTPConfig) string {
+	if cfg.AdvAddr != "" {
+		return cfg.AdvAddr
+	}
+	return cfg.Addr
+}
+
+func logShutdownError(appLog zerolog.Logger, component string, err error) {
+	if err != nil {
+		appLog.Warn().Err(err).Str("component", component).Msg("shutdown error")
+	}
+}
+
 func openLogFile(path string) *os.File {
 	f, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o666)
 	if err != nil {
@@ -222,14 +260,14 @@ func openLogFile(path string) *os.File {
 	return f
 }
 
-func initObservability(ctx context.Context, wireCfg *config.WireConfig) (func(), error) {
+func initObservability(ctx context.Context, appLog zerolog.Logger, wireCfg *config.WireConfig, nodeID string) (func(), error) {
 	shutdown, err := observability.Init(ctx, observability.Config{
 		Enabled:        wireCfg.Observability.Enabled,
 		ServiceName:    "wire-" + wireCfg.Mode,
 		ServiceVersion: cmd.Version,
-		NodeID:         wireCfg.Node.ID,
+		NodeID:         nodeID,
 		MetricsAddr:    wireCfg.Observability.MetricsAddr,
-	}, log.Logger)
+	}, appLog)
 	if err != nil {
 		return nil, err
 	}
@@ -237,7 +275,7 @@ func initObservability(ctx context.Context, wireCfg *config.WireConfig) (func(),
 		shutdownCtx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
 		defer cancel()
 		if err := shutdown(shutdownCtx); err != nil {
-			log.Warn().Err(err).Msg("observability shutdown error")
+			appLog.Warn().Err(err).Msg("observability shutdown error")
 		}
 	}, nil
 }
